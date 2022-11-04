de0df6f4b215268b341dc10f68391ca4d02cf033
Lloyd Montero <catchphilippines@gmail.com>
2022-11-04 21:51:50 +0800

    added file fetching data

--- pages/training/index.js
+++ pages/training/index.js
@@ -3,8 +3,10 @@ import { useRouter } from "next/router";
 import { useEffect } from "react";
 import Layout from "../../components/layout";
 import TrainingComp from "../../components/training";
+import { promises as fs } from "fs";
+import path from "path";
 
-export default function Training() {
+export default function Training({ trainings }) {
     const { status, data } = useSession();
     const router = useRouter();
 
@@ -14,7 +16,8 @@ export default function Training() {
 
     useEffect(() => {
         try {
-          if (status === "unauthenticated") router.replace("/");          
+          if (status === "unauthenticated") router.replace("/");
+          console.log(trainings);   
         } catch(error) {
           console.log(error);
         }
@@ -31,4 +34,25 @@ export default function Training() {
     }
     
     return <div>loading</div>;
+}
+
+export async function getStaticProps() {
+  const trainingDirectory = path.join(process.cwd(), 'module');
+  const filenames = await fs.readdir(trainingDirectory);
+
+  const trainings = filenames.map(async (filename) => {
+    const filePath = path.join(trainingDirectory, filename);
+    const fileContents = await fs.readFile(filePath, 'utf8');
+
+    return {
+      filename,
+      content: fileContents,
+    }
+  })
+
+  return {
+    props: {
+      trainings: await Promise.all(trainings),
+    },
+  }
 }
