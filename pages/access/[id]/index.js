f95f68cf795e99062fddf899ab2c2ca7a80fc41c
Lloyd Montero <catchphilippines@gmail.com>
2023-01-09 21:44:08 +0800

    updated the fetch code method to ssr

--- pages/access/[id]/index.js
+++ pages/access/[id]/index.js
@@ -1,5 +1,5 @@
 import { signOut, useSession } from "next-auth/react";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { useRouter } from "next/router";
 import moment from "moment";
 import Layout from "../../../components/layout";
@@ -30,68 +30,70 @@ export default function RedeemCourses({courses, codes}) {
         return action.data.data[0]._id.toString();
       }
 
-    const getCoursesWithRedeemedCodes = async () => {
+    const getCoursesWithRedeemedCodes = useCallback(async () => {
         let tempRedeems = [];
         let tempCodes = [];
 
-        setIsPageReady(false);
-        courses.map(async (item) => {
-            const cs = await getRedeemByUserIdAndCourseId(router.query.id, item._id);            
-            if (cs.length > 0) {
-                tempRedeems.push({
-                    id: item._id,
-                    redeem_id: cs[0]._id,
-                    title: item.title,
-                    code: cs[0].code,
-                    isRedeemed: cs[0].isRedeemed,
-                    isExpired: cs[0].isExpired,
-                    dateCreated: cs[0].dateCreated
-                })
-            } else {
-                tempRedeems.push({
-                    id: item._id,
-                    redeem_id: "",
-                    title: item.title,
-                    code: "",
-                    isRedeemed: "",
-                    isExpired: "",
-                    dateCreated: ""
-                })
-            }
+        // setIsPageReady(false);
+        // courses.map(async (item) => {
+        //     const cs = await getRedeemByUserIdAndCourseId(router.query.id, item._id);            
+        //     if (cs.length > 0) {
+        //         tempRedeems.push({
+        //             id: item._id,
+        //             redeem_id: cs[0]._id,
+        //             title: item.title,
+        //             code: cs[0].code,
+        //             isRedeemed: cs[0].isRedeemed,
+        //             isExpired: cs[0].isExpired,
+        //             dateCreated: cs[0].dateCreated
+        //         })
+        //     } else {
+        //         tempRedeems.push({
+        //             id: item._id,
+        //             redeem_id: "",
+        //             title: item.title,
+        //             code: "",
+        //             isRedeemed: "",
+        //             isExpired: "",
+        //             dateCreated: ""
+        //         })
+        //     }
 
-            setRedeems(tempRedeems);
-        })
+        //     console.log("Fetching admin redeem codes");
+        //     console.log(tempRedeems);
+        //     // setRedeems(tempRedeems);
+        // })
 
-        codes.map(async (item) => {
-            const cs = await getRedeemByUserIdAndCourseId(router.query.id, item.course_id);
-            const courseTitle = await getCourseById(item.course_id);            
-            if (cs.length > 0) {
-                tempCodes.push({
-                    id: item.course_id,
-                    redeem_id: cs[0]._id,
-                    title: courseTitle.data.data[0].title,
-                    code: cs[0].code,
-                    isRedeemed: cs[0].isRedeemed,
-                    isExpired: cs[0].isExpired,
-                    dateCreated: cs[0].dateCreated
-                })
-            } else {
-                tempCodes.push({
-                    id: item.course_id,
-                    redeem_id: "",
-                    title: courseTitle.data.data[0].title,
-                    code: "",
-                    isRedeemed: "",
-                    isExpired: "",
-                    dateCreated: ""
-                })
-            }
+        // codes.map(async (item) => {
+        //     const cs = await getRedeemByUserIdAndCourseId(router.query.id, item.course_id);
+        //     const courseTitle = await getCourseById(item.course_id);            
+        //     if (cs.length > 0) {
+        //         tempCodes.push({
+        //             id: item.course_id,
+        //             redeem_id: cs[0]._id,
+        //             title: courseTitle.data.data[0].title,
+        //             code: cs[0].code,
+        //             isRedeemed: cs[0].isRedeemed,
+        //             isExpired: cs[0].isExpired,
+        //             dateCreated: cs[0].dateCreated
+        //         })
+        //     } else {
+        //         tempCodes.push({
+        //             id: item.course_id,
+        //             redeem_id: "",
+        //             title: courseTitle.data.data[0].title,
+        //             code: "",
+        //             isRedeemed: "",
+        //             isExpired: "",
+        //             dateCreated: ""
+        //         })
+        //     }
 
-            setStudentRedeems(tempCodes);
-        })
-
-        setIsPageReady(true); // for admin to see the management view
-    }
+        //     console.log("Fetching admin redeem codes");
+        //     console.log(tempCodes);
+        //     // setStudentRedeems(tempCodes);
+        // })
+    }, [])
 
     const goToView = (userId, id) => {
         router.push(`/access/${userId}/view/${id}`);
@@ -101,15 +103,20 @@ export default function RedeemCourses({courses, codes}) {
         try {
             if (status === "unauthenticated") router.replace("/");       
             if (status === "authenticated") {
+                setIsPageReady(false);
                 getRole(data.user.email);     
                 isEmailExisting();           
-                getCoursesWithRedeemedCodes();
+                // getCoursesWithRedeemedCodes();
                 setId(router.query.id);
+                console.log(role);
+                console.log("Fetching codes from ssr...");
+                console.log(codes);
+                setIsPageReady(true); // for admin to see the management view
             }
         } catch (error) {
             console.log(error);
         }
-    }, [status]);
+    }, [status, role]);
 
     if (isPageReady === true) {
         return (
@@ -121,11 +128,11 @@ export default function RedeemCourses({courses, codes}) {
                 />
                 {
                     role === "admin" ?
-                    <CoursesComp redeems={redeems} goToView={goToView} id={id} /> : ""
+                    <CoursesComp redeems={courses} goToView={goToView} id={id} /> : ""
                 }
                 {
                     role === "student" ?
-                    <CoursesComp redeems={studentRedeems} goToView={goToView} id={id} /> : ""
+                    <CoursesComp redeems={codes} goToView={goToView} id={id} /> : ""
                 }
             </div>
         )
@@ -145,27 +152,76 @@ export default function RedeemCourses({courses, codes}) {
 }
 
 export async function getServerSideProps(context) {
+    let tempRedeems = [];
+    let tempCodes = [];
+    
     const courses = await getCourses();
     const codes = await getRedeemByUserId(context.params.id);
 
-    // if (courses.data.data.length > 0) {
-    //     temp.pop();  
-    //     courses.data.data.map(async (item) => {
-    //         const code = await getRedeemByUserIdAndCourseId(context.params.id, item._id);
-    //         temp.push({
-    //             id: code._id,
-    //             // title: item.title,
-    //             // code: code.code,
-    //             // isRedeemed: code.isRedeemed,
-    //             // isExpired: code.isExpired
-    //         })
-    //     })
-    // }
+    // NOT SURE IF WE NEED THIS?
+    // There is no link in admin portal that will lead to this
+    if (courses) {
+        for (let i = 0; i <= parseInt(courses.length) - 1; i++) {
+            const cs = await getRedeemByUserIdAndCourseId(context.params.id, courses[i]._id);
+
+            if (cs.length > 0) {
+                tempRedeems.push({
+                    id: courses[i]._id,
+                    redeem_id: cs[0]._id,
+                    title: courses[i].title,
+                    code: cs[0].code,
+                    isRedeemed: cs[0].isRedeemed,
+                    isExpired: cs[0].isExpired,
+                    dateCreated: cs[0].dateCreated
+                })
+            } else {
+                tempRedeems.push({
+                    id: courses[i]._id,
+                    redeem_id: "",
+                    title: courses[i].title,
+                    code: "",
+                    isRedeemed: "",
+                    isExpired: "",
+                    dateCreated: ""
+                })
+            }
+        }
+    }
+    
+    if (codes) {
+        for (let i = 0; i <= parseInt(codes.length) - 1; i++) {
+            const cs = await getRedeemByUserIdAndCourseId(context.params.id, codes[i].course_id);
+            const courseTitle = await getCourseById(codes[i].course_id);            
+            if (cs.length > 0) {
+                tempCodes.push({
+                    id: codes[i].course_id,
+                    redeem_id: cs[0]._id,
+                    title: courseTitle.data.data[0].title,                    
+                    code: cs[0].code,
+                    isRedeemed: cs[0].isRedeemed,
+                    isExpired: cs[0].isExpired,
+                    dateCreated: cs[0].dateCreated
+                })
+            } else {
+                // NOT SURE IF WE NEED THIS
+                // tempCodes.push({
+                //     id: codes[i].course_id,
+                //     redeem_id: "",
+                //     // title: courseTitle.data.data[0].title,
+                //     title: "Course Sample",
+                //     code: "",
+                //     isRedeemed: "",
+                //     isExpired: "",
+                //     dateCreated: ""
+                // })
+            }
+        }
+    }
 
     return {
         props: {
-            courses: courses.data.data,
-            codes
+            courses: tempRedeems,
+            codes: tempCodes
         }
     }
 }
